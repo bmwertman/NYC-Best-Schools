7651bd885a1af26fa3e94fc48cb98d7bf3e839b5
bmwertman <brad.wertman@leanmeanz.com>
2014-01-13 14:06:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestNycSchools::Application.config.secret_token = '9e9298bd22dc13590a2a4dbea0f8128408d781fde68c3a5874ffe7e93c9559c0fe759e5b6702c54a21a719f86b45392080174293a11f40964f05f34b88c9d87c'
