6ab8e1046473d5daf637018828f9e3721a291a3d
bmwertman <brad.wertman@leanmeanz.com>
2014-01-14 19:50:37 -0500

    routes are broken

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-  require 'csv'
+require 'csv'
 CSV.foreach(File.path(Rails.root.join('lib', "ES_Zones_2013-2014.csv"))) do |column|
   SchoolZone.create({
     shape_length: column[8],
