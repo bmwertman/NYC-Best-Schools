13b703bc4cf9548c885c39f5418e22050438a7d2
bmwertman <brad.wertman@leanmeanz.com>
2014-01-15 14:53:31 -0500

    heroku stanza

--- config/application.rb
+++ config/application.rb
@@ -22,27 +22,27 @@ end
 # Additional info on PostGIS configuration for Heroku at
 # https://devcenter.heroku.com/articles/postgis#setting-up-postgis-with-rails
 # and https://github.com/cyberdelia/heroku-geo-buildpack/
-# class ActiveRecordOverrideRailtie < Rails::Railtie
-#   initializer "active_record.initialize_database.override" do |app|
+class ActiveRecordOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
 
-#     ActiveSupport.on_load(:active_record) do
-#       if url = ENV['DATABASE_URL']
-#         ActiveRecord::Base.connection_pool.disconnect!
-#         parsed_url = URI.parse(url)
-#         config =  {
-#           adapter:             'postgis',
-#           host:                parsed_url.host,
-#           encoding:            'unicode',
-#           database:            parsed_url.path.split("/")[-1],
-#           port:                parsed_url.port,
-#           username:            parsed_url.user,
-#           password:            parsed_url.password
-#         }
-#         establish_connection(config)
-#       end
-#     end
-#   end
-# end
+    ActiveSupport.on_load(:active_record) do
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
 
 module BestNycSchools
   class Application < Rails::Application
